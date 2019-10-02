70e599c9afe12803c8a71f309c4e78099774c2b1
Benson <tumlivein@gmail.com>
2019-10-02 23:23:15 +0800

    32. Customizing HTTP Request: JSON Option

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/37.8267,-122.4233';
 
-request({ url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    // console.log(data);
-    console.log(data.currently);
+request({ url, json: true }, (error, response) => {
+    console.log(response.body.currently);
 });
