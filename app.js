4ee0e215665a0b8473f5776b69c7427810e6fd95
Benson <tumlivein@gmail.com>
2019-10-02 23:30:46 +0800

    Challenge: Get Temp & Rain Chance Back

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/37.8267,-122.4233';
 
 request({ url, json: true }, (error, response) => {
-    console.log(response.body.currently);
+    const { temperature, precipProbability } = response.body.currently;
+    console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`);
 });
+
+// 
+// Goal: Print a small forecast to the user
+// 
+// 1. Print: "It is currently 58.5 degrees out. There is a 0% chance of rain."
+// 2. Test your work!
