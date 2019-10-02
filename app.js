bacc70abf099503b0d5330ef1ce229f4b185c006
Benson <tumlivein@gmail.com>
2019-10-02 23:42:42 +0800

    32. Customizing HTTP Request: Customize Query String

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/37.8267,-122.4233?lang=ko&units=si';
 
 request({ url, json: true }, (error, response) => {
     const { temperature, precipProbability } = response.body.currently;
-    console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`);
+    const { summary } = response.body.daily;
+    console.log(`${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`);
 });
 
-// 
-// Goal: Print a small forecast to the user
-// 
-// 1. Print: "It is currently 58.5 degrees out. There is a 0% chance of rain."
-// 2. Test your work!
