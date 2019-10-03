377982a2b5be6f7990c61d39e24a9e94ba5f8f7f
Benson Chen <tumlivein@gmail.com>
2019-10-03 13:52:44 +0800

    37. Callback Abstraction Challenge

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
 const request = require('request');
 const chalk = require('chalk');
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-// const url = 'https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/37.8267,-122.4233?lang=ko&units=si';
-
-// request({ url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service');
-//     } else if (response.body.error) {
-//         console.log('Unable to find location');
-//     } else {
-//         const { 
-//             currently:  { temperature, precipProbability },
-//             daily:      { summary },
-//         } = response.body;
-    
-//         console.log(`${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`);
-//     }
-// });
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/taichung.json?access_token=pk.eyJ1IjoidHVtbGl2ZWluIiwiYSI6ImNrMWExc29yaTB4YjgzbW4ycmM3dnpyZngifQ.jaDrQvaHZAXResYN_7Th2Q&limit=1';
-
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to location services');
-//     } else if (response.body.features.length === 0) {
-//         console.log('No Place Found!');
-//     } else {
-//         const [ latitude, longitude ] = response.body.features[0].center;
-//         console.log(latitude, longitude);
-//     }
-// });
-
+const errorIcon = (text) => chalk.black.bgRed(` ${text} `);
+const dataIcon = (text) => chalk.black.bgGreen(` ${text} `);
 
 geocode('Taichung', (error, data) => {
-    console.log(chalk.black.bgRed(' Error '), error);
-    console.log(chalk.black.bgGreen(' Data '), data);
+    console.log(errorIcon('Error'), error);
+    console.log(dataIcon('Data'), data);
+});
+
+// 
+// Goal: Create a reusalbe function for getting the forecast 
+// 
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecase function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecase string for data (same format as from before)
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log(errorIcon('Error'), error);
+    console.log(dataIcon('Data'), data);
 });
---
+++ utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const forecast = (lat, lng, callback) => {
+    const url = `https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/${lat},${lng}`;
+
+    request({ url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to the services.', undefined);
+        } else if (response.body.error) {
+            callback('Unable to get the location.', undefined);
+        } else {
+            const data = response.body;
+            const { temperature, precipProbability } = data.currently;
+            const { summary } = data.daily;
+        
+            callback(undefined, `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`);
+        }
+    });
+};
+
+module.exports = forecast;
