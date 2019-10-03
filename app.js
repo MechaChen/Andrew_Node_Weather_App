27b6f9158133812d2332b554fa20c55c3fd908e7
Benson Chen <tumlivein@gmail.com>
2019-10-03 11:47:01 +0800

    36. Callback Abstraction

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const request = require('request');
+const chalk = require('chalk');
+const geocode = require('./utils/geocode');
 
 // const url = 'https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/37.8267,-122.4233?lang=ko&units=si';
 
@@ -17,23 +19,21 @@ const request = require('request');
 //     }
 // });
 
-// 
-// Goal: Handle errors for geocoding request
-// 
-// 1. Setup an error handler for low-level errors
-// 2. Test by disableing network request and running the app
-// 3. Setup error handling for no matching results
-// 4. Test by altering the search term and running the app
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/taichung.json?access_token=pk.eyJ1IjoidHVtbGl2ZWluIiwiYSI6ImNrMWExc29yaTB4YjgzbW4ycmM3dnpyZngifQ.jaDrQvaHZAXResYN_7Th2Q&limit=1';
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/taichung.json?access_token=pk.eyJ1IjoidHVtbGl2ZWluIiwiYSI6ImNrMWExc29yaTB4YjgzbW4ycmM3dnpyZngifQ.jaDrQvaHZAXResYN_7Th2Q&limit=1';
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to location services');
+//     } else if (response.body.features.length === 0) {
+//         console.log('No Place Found!');
+//     } else {
+//         const [ latitude, longitude ] = response.body.features[0].center;
+//         console.log(latitude, longitude);
+//     }
+// });
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location services');
-    } else if (response.body.features.length === 0) {
-        console.log('No Place Found!');
-    } else {
-        const [ latitude, longitude ] = response.body.features[0].center;
-        console.log(latitude, longitude);
-    }
+
+geocode('Taichung', (error, data) => {
+    console.log(chalk.black.bgRed(' Error '), error);
+    console.log(chalk.black.bgGreen(' Data '), data);
 });
